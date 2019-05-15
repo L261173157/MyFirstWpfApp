9cf77c5b06c1fdf44cac1a422df5e40f7e5cf52a
L261173157 <261173157@qq.com>
2019-05-15 16:58:52 +0800

    0515

--- MyFirstWpfApp/MainWindow.xaml.cs
+++ MyFirstWpfApp/MainWindow.xaml.cs
@@ -23,6 +23,19 @@ namespace MyFirstWpfApp
         public MainWindow()
         {
             InitializeComponent();
+            
+        }
+        SolidColorBrush mySolidColorBrush = new SolidColorBrush();
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            mySolidColorBrush.Color = Color.FromArgb(0, 0, 0, 0);
+            Rectangle1.Fill = mySolidColorBrush;
+        }
+
+        private void BlackButton_Click(object sender, RoutedEventArgs e)
+        {
+            mySolidColorBrush.Color = Color.FromArgb(255, 255, 255, 0);
+            Rectangle1.Fill = mySolidColorBrush;
         }
     }
 }
