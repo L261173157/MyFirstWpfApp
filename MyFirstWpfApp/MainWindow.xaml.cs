10964a319500874f0029aa31dc806f7b47db2595
L261173157 <261173157@qq.com>
2019-05-16 16:56:15 +0800

    0516

--- MyFirstWpfApp/MainWindow.xaml.cs
+++ MyFirstWpfApp/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Common.Utility;
+using System.ComponentModel;
 
 namespace MyFirstWpfApp
 {
@@ -20,22 +22,84 @@ namespace MyFirstWpfApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        int Age;
+        Student student;
+        public static string myString = "abc";
+        List<employee> employees = new List<employee>()
+        {
+            new employee(){Id=2,Name="lin",Age=10 },
+            new employee(){Id=1,Name="wen",Age=11}
+        };
         public MainWindow()
         {
-            InitializeComponent();
             
+            InitializeComponent();
+            this.ListBoxEmployee.DisplayMemberPath = "Name";
+            this.ListBoxEmployee.SelectedValuePath = "Id";
+            this.ListBoxEmployee.ItemsSource = employees;
+            //student = new Student();
+            //Binding binding = new Binding();
+            //binding.Source = student;
+            //binding.Path = new PropertyPath("Name");
+            //BindingOperations.SetBinding(this.textBoxName, TextBox.TextProperty, binding);
+            this.textBoxName.SetBinding(TextBox.TextProperty, new Binding("Name") { Source = student = new Student() });
+
         }
         SolidColorBrush mySolidColorBrush = new SolidColorBrush();
+
+        public static string MyString { get => myString; set => myString = value; }
+        
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             mySolidColorBrush.Color = Color.FromArgb(0, 0, 0, 0);
             Rectangle1.Fill = mySolidColorBrush;
+            myString = "white";
+
         }
 
         private void BlackButton_Click(object sender, RoutedEventArgs e)
         {
             mySolidColorBrush.Color = Color.FromArgb(255, 255, 255, 0);
             Rectangle1.Fill = mySolidColorBrush;
+            myString = "black";
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            employees.Add(new employee() { Id = 3, Name = "li", Age = 12 });
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            student.Name += "Name";
         }
     }
+    public class employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+    class Student:INotifyPropertyChanged
+    {
+        string name;
+
+        public string Name
+        {
+            get => name;
+            set
+            {
+                name = value;
+                
+                if(this.PropertyChanged!=null)
+                {
+                    this.PropertyChanged.Invoke(this,new PropertyChangedEventArgs ("Name"));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+    }
+    
 }
